f86877ac9a6b788a92eb2d14b3829699d0231531
sagarsingh0304 <sagar.singh0304@gmail.com>
2021-06-19 11:11:33 +0530

    added Procfile and gevents and websocket server for heroku

--- server.py
+++ server.py
@@ -96,5 +96,5 @@ def on_leave(game_id, name):
 
 
 if __name__ == "__main__":
-    # app.run(debug=True, port=5000)
-    socketio.run(app, debug=True, port=5000)
+    app.run()
+    # socketio.run(app, debug=True, port=5000)
